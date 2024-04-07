6848b7ce5e07cc17a8f1aeacff0486a16873c758
unknown <8mau.coronel@gmail.com>
2024-04-07 10:21:31 -0600

    Getting the list of birthdays

--- src/components/ListBirthday.js
+++ src/components/ListBirthday.js
@@ -1,16 +1,38 @@
 /* eslint-disable prettier/prettier */
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import {getDocs, collection} from 'firebase/firestore';
+
+import {db} from '../utils/firebase';
 
 import ActionBar from './ActionBar';
 import AddBirthday from './AddBirthday';
 
-export default function ListBirthday() {
+export default function ListBirthday({user}) {
   const [showList, setShowList] = useState(true);
+  const [birthday, setBirthday] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const itemsArray = [];
+      const querySnap = await getDocs(collection(db, user.uid));
+      querySnap.forEach(doc => {
+        const data = doc.data();
+        itemsArray.push(data);
+      });
+      setBirthday(itemsArray);
+    };
+
+    fetchData();
+  }, [user.uid]);
 
   return (
     <View style={styles.container}>
-      {showList ? <Text>Birthday</Text> : <AddBirthday />}
+      {showList ? (
+        <Text>Birthday</Text>
+      ) : (
+        <AddBirthday user={user} setShowList={setShowList} />
+      )}
       <ActionBar showList={showList} setShowList={setShowList} />
     </View>
   );
